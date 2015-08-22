edbfc574db46d3e05d7f82ceca26d820ef4d06f8
Keith Pinson <me@keithpinson.com>
2015-08-22 19:49:50 -0400

    Starting to code the tests for markdown

---
+++ src/test/scala/com/keithpinson/gobetweentest/UrlTest.scala
@@ -0,0 +1,18 @@
+package com.keithpinson.gobetweentest
+
+/*
+ * Copyright (c) 2015 Keith Pinson.
+ *
+ * Created: 8/22/2015
+ */
+
+/**
+ * Test the Url class using ScalaCheck and Specs2 as specified in the URL Requirements.
+ *
+ * @see [[com.keithpinson.gobetween.TermsAndConditions]]<br/>
+ *
+ * @author [[http://keithpinson.com Keith Pinson]]
+ */
+class UrlTest {
+
+}
