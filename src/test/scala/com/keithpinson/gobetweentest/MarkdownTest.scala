450f5af4a8c2aba5a1c38d9fde8d307030e72bad
Keith Pinson <me@keithpinson.com>
2015-09-03 21:58:31 -0400

    Added further tests

--- src/test/scala/com/keithpinson/gobetweentest/MarkdownTest.scala
+++ src/test/scala/com/keithpinson/gobetweentest/MarkdownTest.scala
@@ -26,9 +26,35 @@ class MarkdownTest extends Specification { def is = sequential ^
 
 trait MarkdownTestHelpers {
   def genUnicode : Gen[Char] = for { c <- Gen.chooseNum(0x0000,0x26FF) } yield c.toChar
-  def genLine : Gen[String] = for { cc <- Gen.nonEmptyListOf(Gen.alphaNumChar); e <- Gen.oneOf("\n","\r","\r\n") } yield cc.take(80).mkString + e
+  def genLine : Gen[String] = for { cc <- Gen.nonEmptyListOf(Gen.alphaNumChar); e <- genEOL } yield cc.take(80).mkString + e
+  def genEOL : Gen[String] = for { cc <- Gen.oneOf("\n","\r","\r\n") } yield cc
+  def genBlankLine : Gen[String] = for { ww <- genWhitespace; cc <- genEOL } yield ww + cc
+  def genWhitespace : Gen[String] = for { ww <- Gen.listOf(for {w <- Gen.oneOf(ws)} yield w) } yield ww.take(60).mkString
 
-//  def genSafeString : Gen[String] = for { cc <- Gen.nonEmptyListOf(for {n <- Gen.chooseNum(0x0000,0x26FF)} yield n.toChar) } yield cc.take(maxLength).mkString
+  private val ws = List(
+    "0x0009", // horizontal tab
+    "0x000B", // vertical tab
+    "0x000C", // form feed
+    "0x0020", // space
+    "0x00A0", // no-break space
+    "0x1680", // ogham space mark
+    "0x2000", // en quad
+    "0x2001", // em quad
+    "0x2002", // en space
+    "0x2003", // em space
+    "0x2004", // three-per-em space
+    "0x2005", // four-per-em space
+    "0x2006", // six-per-em-space
+    "0x2007", // figure space
+    "0x2008", // punctuation space
+    "0x2009", // thin space
+    "0x200A", // hair space
+    "0x202F", // narrow no-break space
+    "0x205F", // medium mathematical space
+    "0x3000" // ideographic space
+  )
+
+  //  def genSafeString : Gen[String] = for { cc <- Gen.nonEmptyListOf(for {n <- Gen.chooseNum(0x0000,0x26FF)} yield n.toChar) } yield cc.take(maxLength).mkString
 
 }
 
@@ -40,24 +66,26 @@ class MarkdownTerminologyTest extends Specification with ScalaCheck with Markdow
 
   A line is a sequence of zero or more characters followed by an end of line $checkLine
 
+  An end of line is one of the following:
+      An end of File
+      A newline (U+000A)
+      A carriage return (U+000D)
+      Or, a carriage return + newline (U+000D,U+000A) $checkEOL
+
+  A line containing no characters after all the whitespace has been removed is a blank line $checkBlankLine
+
 """
 
   def checkCharacter = prop( (c:Char) => c must beBetween(0x0000.toChar,0x26FF.toChar) ).setGen(genUnicode)
 
-  def checkLine = prop( (s:String) => s.last must beOneOf( '\n', '\r' ) ).setGen(genLine)
+  def checkLine = prop( (s:String) => s must beMatching(""".*(\n|\r|\r\n)$""") ).setGen(genLine)
+
+  def checkEOL = prop( (s:String) => s.lengthCompare(2) <= 0 && (s must beMatching("""(\n|\r|\r\n)$""")) ).setGen(genEOL)
+
+  def checkBlankLine = prop( (s:String) => s must beMatching("""^[\p{Zs}]*(\n|\r|\r\n)$""") ).setGen(genBlankLine)
 }
 
 /*
-    [[Lines]]
-    A line is a sequence of zero or more characters followed by an end of line[End of Line]]
-
-    [[End of Line]]
-    An end of line is one of the following:
-        An end of File
-        A newline (U+000A)
-        A carriage return (U+000D)
-        Or, a carriage return + newline (U+000D,U+000A)
-
     [[Blank Line]]
     A line containing no characters after all the whitespace has been removed is a blank line[[Whitespace]]
 
