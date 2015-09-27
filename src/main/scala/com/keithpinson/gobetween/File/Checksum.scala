6ab99a36c048a28073490387fcf75fe37ac4a8b1
Keith Pinson <me@keithpinson.com>
2015-09-26 22:02:01 -0400

    Finished expiration date check

--- src/main/scala/com/keithpinson/gobetween/File/Checksum.scala
+++ src/main/scala/com/keithpinson/gobetween/File/Checksum.scala
@@ -30,28 +30,26 @@ import java.text.SimpleDateFormat
 class Checksum( secretKey:String ) {
 
   // Current version should be at the head position
-  final private val versions = Map(
+  final private val versions = scala.collection.SortedMap(
     'B'.toByte -> "2035-12-31T23:59:59Z",
     'A'.toByte -> "2000-01-01T00:00:00Z"  // Expired
   )
-
-  private val dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
 
   private def hashB( bb:Array[Byte], ver:Char ) = {
     val mac = Mac.getInstance("HmacSHA256")
 
     mac.init(new SecretKeySpec(secretKey.getBytes("utf-8"), "HmacSHA256"))
 
-    new String(ver +: mac.doFinal(bb))
+    new String(ver.toByte +: mac.doFinal(bb))
   }
 
 
   def checksum( encryptedString:String ) : String = checksum( encryptedString.getBytes("utf-8") )
 
   def checksum( encryptedBytes:Array[Byte] ) : String = {
-    val csVersion = encryptedBytes.head
+    val csVersion = versions.last._1
 
-    csVersion.toChar match {
+    csVersion match {
       case 'B' => hashB( encryptedBytes, 'B' )
       case _ => "A"
     }
@@ -59,14 +57,16 @@ class Checksum( secretKey:String ) {
 
   def verify( encryptedString:String, cs:String ) : Boolean = verify( encryptedString.getBytes("utf-8"), cs )
 
-  def verify( encryptedBytes:Array[Byte], cs:String ) : Boolean = {
-    val csVersion = encryptedBytes.head
-    val now = new SimpleDateFormat(dateFormat).format(new Date(System.currentTimeMillis + 60 * 60 * 1000))
+  def verify( encryptedBytes:Array[Byte], cs:String ) : Boolean = ! isExpired( cs ) && checksum(encryptedBytes) == cs
 
-    val dt = new SimpleDateFormat(dateFormat)
+  private def isExpired( cs:String ) : Boolean = {
+    val csVersion = cs.head
+    val dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    val now = new Date(System.currentTimeMillis + 60 * 60 * 1000)
 
-    csVersion.toChar match {
-      case 'B' => dt.parse(versions.getOrElse('B'.toByte, "2000-01-01T00:00:00Z")); dt < now && checksum(encryptedBytes.tail) == cs
-      case _ => false
+    csVersion match {
+      case 'B' => dateFormat.parse(versions.getOrElse('B'.toByte, "2000-01-01T00:00:00Z")).compareTo(now) < 0
+      case _ => true
+    }
   }
 }
