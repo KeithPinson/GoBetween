bfc707e6afd13346c06de44e40cb6bf05d3a3f0e
Keith Pinson <me@keithpinson.com>
2016-01-19 20:46:24 -0500

    Fixed reference

---
+++ src/main/scala/com/keithpinson/gobetween/File/HammingLike.scala
@@ -0,0 +1,26 @@
+package com.keithpinson.gobetween.File
+
+/*
+ * Copyright (c) 2015 Keith Pinson.
+ *
+ * Created: 9/13/2015
+ */
+
+/**
+ * This...
+ *
+ * @see [[com.keithpinson.gobetween.TermsAndConditions]]<br/>
+ *
+ * @author [[http://keithpinson.com Keith Pinson]]
+ */
+class HammingLike {
+
+  // Most Frequent k Chars
+
+  // Tversky Index
+
+  // Jaccard Index
+
+
+
+}
